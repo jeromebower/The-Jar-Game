6a9a91080cad84298f9ffea530c2d986a1890c9d
jeromebower <jerome.bower@yahoo.com>
2017-01-28 22:55:53 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,37 +1,11 @@
-import java.util.Scanner; 
-
 public class Game {
-   
-    public static void main(String[] args) {
-            
-        String mTypeOfItem;
-        int mMaxNumber;
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("What type of item? ");
-        System.out.printf("%n");
-        mTypeOfItem = scanner.next();  
-        System.out.print("How many will fit in the jar? ");
-        System.out.printf("%n");
+  
+  public static void main(String[] args) {  
 
-        do {
-          while (!scanner.hasNextInt()) {
-            String input = scanner.next();
-            System.out.printf("%s is not a valid number.\n", input);
-            System.out.printf("Pick a number: ");
-          }
-          mMaxNumber = scanner.nextInt();
-          if(mMaxNumber == 0){
-            System.out.printf("Number can not be 0, pick a new number.\n");
-            System.exit(0);
-          }
-        } while (mMaxNumber == 0);
-
-      Jar jar = new Jar(mTypeOfItem, mMaxNumber);
-      
-      Prompter prompter = new Prompter(jar);
-      do{
-        prompter.play(); 
-      }while(prompter.promptNewGame());              
-    }
+    Prompter prompter = new Prompter();
+    Jar jar = new Jar(prompter.gameType(), prompter.gameMaxNumber()); 
+    do{    
+      prompter.play(jar); 
+    }while(prompter.promptNewGame());              
+  }
 }
